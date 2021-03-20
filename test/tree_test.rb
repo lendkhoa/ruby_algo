e18e8e0fe8d8241500e4e423e573b5376f2f8b4f
Khoa Le <kdl2483@truman.edu>
2021-03-20 15:17:56 -0500

    Update test

--- test/tree_test.rb
+++ test/tree_test.rb
@@ -4,20 +4,20 @@ require './lib/Tree'
 
 class TreeTest < Minitest::Test
   def test_valid_binary_tree
-    root = Tree::Tree_Node.new(2)
-    root.left = Tree::Tree_Node.new(1, nil, nil)
-    root.right = Tree::Tree_Node.new(3, nil, nil)
-    root.right.right = Tree::Tree_Node.new(4, nil, nil)
-    Tree::Binary_Tree.new(root)
+    root = Tree::TreeNode.new(2)
+    root.left = Tree::TreeNode.new(1, nil, nil)
+    root.right = Tree::TreeNode.new(3, nil, nil)
+    root.right.right = Tree::TreeNode.new(4, nil, nil)
+    Tree::BinaryTree.new(root)
   end
 
   def test_invalid_binary_tree
-    root = Tree::Tree_Node.new(2)
-    root.left = Tree::Tree_Node.new(1, nil, nil)
-    root.right = Tree::Tree_Node.new(3, nil, nil)
-    root.right.right = Tree::Tree_Node.new(3, nil, nil)
+    root = Tree::TreeNode.new(2)
+    root.left = Tree::TreeNode.new(1, nil, nil)
+    root.right = Tree::TreeNode.new(3, nil, nil)
+    root.right.right = Tree::TreeNode.new(3, nil, nil)
     assert_raises Exception do
-      Tree::Binary_Tree.new(root)
+      Tree::BinaryTree.new(root)
     end
   end
 end
